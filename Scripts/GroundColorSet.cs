6a1a08bbe9877b8f151a1bba51cd49ebd9a06482
GrapesOne <35427288+GrapesOne@users.noreply.github.com>
2018-06-25 10:21:58 +0500

    BackgroundGradient and Animator

---
+++ Scripts/GroundColorSet.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+[System.Serializable]
+public class GroundColorSet {
+	public string Name;
+	public Sprite[] Set = new Sprite[0];
+}
---
+++ Scripts/GroungAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+
+public class GroungAnimation : MonoBehaviour {
+	public GroundColorSet[] Sets;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
