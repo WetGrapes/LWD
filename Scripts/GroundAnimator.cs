1f27227114fc774473b9867594c908c3d46debfe
GrapesOne <35427288+GrapesOne@users.noreply.github.com>
2018-06-26 08:10:13 +0500

    GroundAnimator
    Анимация земли с переключением на лету

---
+++ Scripts/GroundAnimator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundAnimator : MonoBehaviour {
+	public GroundColorSet[] Sets;
+	[Range(0, 6)] public int NowSet;
+	[Range(1, 100)] public int MaxFactor=50, MinFactor =20;
+}
--- Scripts/GroungAnimation.cs
+++ Scripts/GroungAnimation.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 using UnityEngine.Sprites;
 
 public class GroungAnimation : MonoBehaviour {
-	public GroundColorSet[] Sets;
+	
+	SpriteRenderer Render;
+	GroundAnimator Animator;
 
+	float MaxSpeed = 3f, MinSpeed = 0.2f;
 
 	void Start () {
-		
+		Render = GetComponent<SpriteRenderer> ();
+		Animator = GameObject.Find("GroundAnimator").GetComponent<GroundAnimator> ();
+		StartCoroutine(ChangeView ());
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+	IEnumerator ChangeView()
+	{
+		MaxSpeed = 0.05f * Animator.MaxFactor;
+		MinSpeed = 0.05f * Animator.MinFactor;
+		Render.sprite = Animator.Sets [Animator.NowSet].Set [Random.Range (0, Animator.Sets [Animator.NowSet].Set.Length)];
+		yield return new WaitForSeconds (Random.Range (MinSpeed, MaxSpeed));
+		StartCoroutine (ChangeView ());
 	}
 }
