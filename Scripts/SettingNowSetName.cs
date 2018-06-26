b464475b4020d9337b4fba7362a497bcfe877315
GrapesOne <35427288+GrapesOne@users.noreply.github.com>
2018-06-26 12:19:44 +0500

    Frame
    Изменил насройки, добавил рамки, расширил настройки

---
+++ Scripts/SettingNowSetName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingNowSetName : MonoBehaviour {
+	GroundAnimator Animator;
+	Text txt;
+	void Start () {
+		Animator = GameObject.Find("GroundAnimator").GetComponent<GroundAnimator> ();
+		txt = GetComponent<Text> ();
+	}
+
+	public void SetText()
+	{
+		if (Animator!= null && txt != null)
+			txt.text = Animator.Sets [Animator.NowSet].Name;
+	}
+}
