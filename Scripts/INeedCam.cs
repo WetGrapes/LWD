076a0ecbeefae6c67337a3d37f8b54ae6aa23d6f
GrapesOne <35427288+GrapesOne@users.noreply.github.com>
2018-06-28 12:23:22 +0500

    UIBehaviour

---
+++ Scripts/INeedCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class INeedCam : UIBehaviour {
+
+	protected CameraManager Cam;
+
+	protected override void WithStartAnother()
+	{
+		FoundCam ();
+	}
+
+	protected void StateToCamera(int State)
+	{ Cam.nowTarget = State;}
+
+	protected void FoundCam()
+	{
+		Cam =  GameObject.Find ("Main Camera").GetComponent<CameraManager> ();
+	}
+
+}
