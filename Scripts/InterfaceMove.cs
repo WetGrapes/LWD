245a0a5d1b11d0805ee72bb919645df0aff04fb9
GrapesOne <35427288+GrapesOne@users.noreply.github.com>
2018-06-28 22:29:12 +0500

    InterfaceMove

---
+++ Scripts/InterfaceMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceMove : MonoBehaviour {
+
+
+	[SerializeField]float LowestPoint = 0f;
+	GameObject Player;
+
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+
+	}
+
+	void Update () {
+		if (Player.transform.position.y < LowestPoint)
+			LowestPoint = Player.transform.position.y;
+		if (gameObject.transform.position.y >= LowestPoint) {
+			float mult = (gameObject.transform.position - Player.transform.position).y
+			             * (gameObject.transform.position - Player.transform.position).y;
+			gameObject.transform.position -= new Vector3 (0, Time.deltaTime * 15 * mult, 0);
+		}
+	}
+}
