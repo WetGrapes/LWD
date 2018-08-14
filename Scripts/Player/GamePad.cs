78dd828c45d6b2209120e5d1bad3ef9084258e26
GrapesOne <35427288+GrapesOne@users.noreply.github.com>
2018-08-14 15:33:27 +0500

    fap

--- Scripts/Player/GamePad.cs
+++ Scripts/Player/GamePad.cs
@@ -17,7 +17,6 @@ public class GamePad : IClickableBotton {
 			switch (gameObject.name) {
 			case "UpBotton":
 				player.Jump = true;
-				Invoke ("Off", 0.1f);
 				break;
 			case "LeftBotton":
 				player.Left = true;
@@ -27,45 +26,24 @@ public class GamePad : IClickableBotton {
 				break;
 			case "LeftUpBotton":
 				player.UpLeft = true;
-				Invoke ("Off", 0.1f);
 				break;
 			case "RightUpBotton":
 				player.UpRight = true;
-				Invoke ("Off", 0.1f);
 				break;
 			case "CentralBotton":
 				player.Central = true; 
 				break;
 			}
-		}
-	}
-
-	void OnMouseUp() {
-		 
-		switch (gameObject.name) {
-	
-		case "LeftBotton":
-			player.Left = false;
-			break;
-		case "RightBotton":
-			player.Right = false;
-			break;
 
 		}
 	}
 
-	void Off(){
-		switch (gameObject.name) {
-		case "UpBotton":
-			player.Jump = false;
-			break;
-		case "LeftUpBotton":
-			player.UpLeft = false;
-			break;
-		case "RightUpBotton":
-			player.UpRight = false;
-			break;
-		}
+	void Send(string name)
+	{
+		if (gameObject.name == name + "Botton")
+			;
+		//3	player.
+		
 	}
 
 
